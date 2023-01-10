6e38ef0befd8517992ee6411b37dddb0571cc8e1
Kirill <snamas@rambler.ru>
2023-01-10 22:52:25 +0700

    День 5, задание 1. Создал класс Car с переменными модель, цвет, год. Сделал сеттеры и геттеры и метод для вывода строки. В классе Task1 создал 3 авто, передал им значения и вывел в консоль. задание 2. Создал класс Motorbike с переменными модель, цвет, год. Сделал  геттеры и метод для вывода строки. В классе Task2 создал 3 байка, передал им значения и вывел в консоль.

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,57 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setModel("Жигули");
+        car1.setColor("Черный");
+        car1.setYear(1989);
+        System.out.println(car1);
+        Car car2 = new Car();
+        car2.setModel("Лада");
+        car2.setColor("Красный");
+        car2.setYear(2017);
+        System.out.println(car2);
+        Car car3 = new Car();
+        car3.setModel("Audi");
+        car3.setColor("Белый");
+        car3.setYear(2022);
+        System.out.println(car3);
+
 
     }
+}
+
+class Car {
+    private String model;
+    private String color;
+    private int year;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String carModel) {
+        model = carModel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String carColor) {
+        color = carColor;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int carYear) {
+        year = carYear;
+    }
+
+    public String toString() {
+        return "Модель авто: " + getModel() + "\nЦвет авто: " + getColor() + "\nГод выпуска: " + getYear();
+    }
+
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,46 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike bike1= new Motorbike("Сузуки", "Красный", 2009);
+        System.out.println(bike1);
+        Motorbike bike2= new Motorbike("Кавасаки", "Зеленый", 2012);
+        System.out.println(bike2);
+        Motorbike bike3= new Motorbike("Харлей", "Черный", 2017);
+        System.out.println("Модель: " + bike3.getModel()+ "\nЦвет: " + bike3.getColor() + "\nГод: " + bike3.getYear());
+
 
     }
+}
+class Motorbike {
+    private String model;
+    private String color;
+    private int year;
+
+    public Motorbike(String model, String color, int year) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+
+
+    public String getColor() {
+        return color;
+    }
+
+
+
+    public int getYear() {
+        return year;
+    }
+
+
+    public String toString() {
+        return "Модель мотоцикла: " + getModel() + "\nЦвет мотоцикла: " + getColor() + "\nГод выпуска: " + getYear();
+    }
+
 }
