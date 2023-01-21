54b3ee67d42a2c8ea36d87936366a18c7ea4156b
Kirill <snamas@rambler.ru>
2023-01-21 20:56:58 +0700

    Day8 StringBuilder, Форматирование строк в Java, Класс Object и метод toString()

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,34 @@
+package day8;
+
+public class Airplane {
+    private String manufacturer;
+    private int year;
+    private int length;
+    private int weight;
+    private int fuel;
+
+    public Airplane(String manufacturer, int year, int length, int weight) {
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.fuel=0;
+    }
+    public int getFuel() {return fuel;}
+//    public void setManufacturer(String manufacturer) {this.manufacturer = manufacturer;}
+//    public void setYear(int year) {this.year = year;}
+//    public void setLength(int length) {this.length = length;}
+//    public void setWeight(int weight) {this.weight = weight;}
+//    public void setFuel(int fuel) {this.fuel =fuel;}
+    public void fillUp(int n){
+        fuel =fuel + n;}
+    public void info(){
+        System.out.println("Изготовитель: " + manufacturer +", год выпуска: " + year +
+                ", длина: " + length + ", вес: " + weight + ", количество топлива в баке: " + fuel);
+    }
+    public String toString() {
+        return "Изготовитель: ".concat(manufacturer).concat(", год выпуска: " + year)
+                .concat(", длина: " + length).concat(", вес: " + weight).concat(", количество топлива в баке: " + fuel);
+
+    }
+}
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,22 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String numbers = "";
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            numbers += i + " ";
+        }
+        long stopTime = System.currentTimeMillis();
+        System.out.println("\nДлительность: " + (stopTime - startTime));
+        System.out.println(numbers);
 
+        StringBuilder num = new StringBuilder("");
+        startTime = System.currentTimeMillis();
+        for(int i=0; i<=20000;i++){
+           num.append(i).append(" ");
+        }
+        stopTime=System.currentTimeMillis();
+        System.out.println("\nДлительность: " + (stopTime - startTime));
+        System.out.println(num);
     }
 }
