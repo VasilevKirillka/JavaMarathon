6281be9c138fe49c6dfb708c3cd0e85b8c6e73f4
Kirill <snamas@rambler.ru>
2023-01-21 21:09:41 +0700

    Day15 Запись в файл

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,38 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/main/resources/shoes.csv");
+        File file2 =new File("data");
+        List<String> shoes= new ArrayList<>();
+        PrintWriter pw=  new PrintWriter(file2);
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] allShoes = line.split(";");
+
+
+                if (Integer.parseInt(allShoes[2]) == 0) {
+                    shoes.add(line);
+                }
+            }
+                for(int i=0; i < shoes.size(); i++){
+                        pw.println(shoes.get(i));}
+                        pw.close();
+
+            } catch(FileNotFoundException e){
+                throw new RuntimeException(e);
+            }
+        }
     }
-}
+
