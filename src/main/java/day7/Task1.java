9aca5e6b15a8923994c4ff412b585bc61eee1c79
Kirill <snamas@rambler.ru>
2023-01-21 20:54:58 +0700

    Day7 static final

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -2,6 +2,12 @@ package day7;
 
 public class Task1 {
     public static void main(String[] args) {
+Airplane airplane1=new Airplane("",0, 79,15000);
+airplane1.setManufacturer("Боинг");
 
+Airplane airplane2=new Airplane("",0,77,17000);
+        airplane2.setManufacturer("Эйрбас");
+
+        Airplane.compareAirplanes(airplane1,airplane2);
     }
 }
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,31 @@
 package day7;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+        Random random=new Random();
+        int r=random.nextInt(21)+90;
+        Player player1=new Player(r);
+        Player player2=new Player(r);
+        Player player3=new Player(r);
+
+        Player.info();
+        Player player4=new Player(r);
+        Player player5=new Player(r);
+        Player player6=new Player(r);
+
+        Player.info();
+
+        Player player7=new Player(r);
+
+        Player.info();
+
+        for(int i=0; i < r; i++)
+        player3.run();
+
+        Player.info();
+
 
     }
 }
