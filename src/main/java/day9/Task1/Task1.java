a3a1b9ba3417fa95d0cbef6a40fa4e78f67fb7fd
Kirill <snamas@rambler.ru>
2023-01-21 20:59:16 +0700

    Day9 Наследование, Полиморфизм, Абстрактные классы

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -3,5 +3,9 @@ package day9.Task1;
 public class Task1 {
     public static void main(String[] args) {
 
+        Student st1=new Student("Кирилл", "Java разработка");
+        Teacher teacher=new Teacher("Чип Дейлович", "Мультведение");
+        st1.printInfo();
+        teacher.printInfo();
     }
 }
---
+++ src/main/java/day9/Task2/Circle.java
@@ -0,0 +1,20 @@
+package day9.Task2;
+
+public class Circle extends Figure{
+    private double r;
+
+    public Circle(double r,String color) {
+        super(color);
+        this.r=r;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI*(r*r);
+    }
+
+    @Override
+    public double perimeter() {
+        return 2*Math.PI*r;
+    }
+}
